3e88894a036ef51483c2cd6c105730dcae36b3d9
lara-ocon <202115710@alu.comillas.edu>
2022-11-06 15:17:16 +0100

    Exportada la salida a formato json

---
+++ Tarea1_bloque3/git_leaks_bloque3.py
@@ -0,0 +1,121 @@
+#! user/bin/python3 
+
+# Buscador de leaks en commits de git - Adquisición de Datos
+# Lara Ocón Madrid - 202115710
+
+from git import Repo
+
+import re, signal, sys , time
+
+import pandas as pd
+
+from tkinter import *
+from tkinter import ttk
+
+import json
+
+
+def handler_signal(signal, frame):
+    # Esta función controla la salida del programa por una señal de
+    # ctrl+C
+    print("\n\n [!] Out .......\n")
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, handler_signal)
+
+
+def step():
+    # Esta funcion muestra el progreso del programa, imprimiendo
+    # dicho progreso en una barra que se va actualizando a medida
+    # que superamos las fases de la ETL
+
+    my_progress['value'] += 0
+    my_label.config(text= "Iniciando ETL")
+    root.update()
+    time.sleep(1)
+
+    # EXTRACCIÓN DE DATOS ======================================#
+    # extraemos los datos del directorio y actualizamos la barra 
+    # de progreso un 33%
+    commit_messages = extract(REPO_DIR)
+    my_progress['value'] += 100 / 3
+    my_label.config(text= "Datos extraídos")
+    root.update()
+    time.sleep(1)
+
+    # TRANSFORMACIÓN DE LOS DATOS ==============================#
+    # Transformamos los datos obteniendo los leaks en los mensajes 
+    # de commits que contengan las palabras clave. Una vez hecho esto
+    # actualizamos la barra de progreso sumando un 33%
+    leaks_encontrados = transform(commit_messages, kwords)
+    my_progress['value'] += 100 / 3
+    my_label.config(text= "Datos transformados")
+    root.update()
+    time.sleep(1)
+
+    # CARGA DE DATOS ==========================================#
+    # Cargamos los datos imprimiéndolos por pantalla y una vez hecho
+    # esto, actualizamos la barra de progreso y esperamos 3 segundos
+    # para cerra la ventana de dicha barra
+    load(leaks_encontrados)
+    my_progress['value'] += 100 / 3
+    my_label.config(text= "Finalizada carga de datos")
+    root.update()
+    time.sleep(3)
+
+    sys.exit(1)
+    
+
+def extract(url):
+    # Función de extracción: creamos el repositorio para trabajar
+    # y devolvemos una lista con los commits en la rama develop
+
+    repo = Repo(url)
+    lista = list(repo.iter_commits('develop'))
+
+    return lista
+
+
+def transform(lista_commits, kwords):
+    # Finción transformación: Recorremos los mensaje de commits en 
+    # busca de las palabras clave y guardamos los mensajes que 
+    # contengan dichas palabras en una lista que devolveremos como 
+    # output
+
+    leaks_encontrados = []
+
+    for commit in lista_commits:
+        for kword in kwords:
+            if re.search(kword, commit.message, re.I):
+                leaks_encontrados.append(commit.message)
+
+    return leaks_encontrados 
+
+def load(leaks_encontrados):
+    data = {}
+    data['leaks encontrados'] = leaks_encontrados
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+if __name__ == "__main__":
+
+    REPO_DIR = "./skale/skale-manager"
+    kwords = ['credentials', 'password','key']
+
+    root = Tk()
+    root.title('ETL leaks en commits')
+    # root.iconbitmap('c:/gui/codemy.ico')
+    root.geometry("500x200")
+    
+    # Barra de progreso
+    my_progress = ttk.Progressbar(root, orient=HORIZONTAL, length=400, mode='determinate')
+    my_progress.pack(pady=20)
+
+    my_label = Label(root, text=" ")
+    my_label.pack(pady=20)
+
+    my_button = Button(root, text = "Iniciar ETL", command=step)
+    my_button.pack(pady=20)
+
+
+    root.mainloop()
